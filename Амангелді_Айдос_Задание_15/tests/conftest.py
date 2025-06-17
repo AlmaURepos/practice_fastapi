0f3ec6ae6e4c78107c41ec59692a64210b68fbe5
amangeldiaidos660 <amangeldiaidos660@gmail.com>
2025-06-17 14:11:39 +0500

    github ci/cd test

--- Амангелді_Айдос_Задание_15/tests/conftest.py
+++ Амангелді_Айдос_Задание_15/tests/conftest.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
