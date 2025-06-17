1879a6b858f13dade652a83fe100a10152954682
amangeldiaidos660 <amangeldiaidos660@gmail.com>
2025-06-17 14:20:51 +0500

    github ci/cd test

--- Амангелді_Айдос_Задание_15/tests/conftest.py
+++ Амангелді_Айдос_Задание_15/tests/conftest.py
@@ -7,35 +7,35 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app import app
 from database import Base, get_session
 
-TEST_DB_URL = os.environ.get("DATABASE_URL")
+TEST_DB_URL = os.getenv("DATABASE_URL")
 
+@pytest_asyncio.fixture(scope="function")
+async def engine_and_session():
+    test_engine = create_async_engine(TEST_DB_URL, future=True, echo=False)
+    test_sessionmaker = sessionmaker(test_engine, expire_on_commit=False, class_=AsyncSession)
 
-test_engine = create_async_engine(TEST_DB_URL, echo=False, future=True)
-test_async_session = sessionmaker(test_engine, class_=AsyncSession, expire_on_commit=False)
-
-
-async def override_get_session():
-    async with test_async_session() as session:
-        yield session
-
-app.dependency_overrides[get_session] = override_get_session
-
-
-@pytest_asyncio.fixture(scope='function', autouse=True)
-async def setup_db():
     async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    yield
+
+    yield test_engine, test_sessionmaker
+
     async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
+    await test_engine.dispose()
 
 @pytest_asyncio.fixture(scope="function")
-async def client():
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as c:
-        yield c
+async def client(engine_and_session):
+    engine, session_maker = engine_and_session
+
+    async def override_get_session():
+        async with session_maker() as session:
+            yield session
+
+    app.dependency_overrides[get_session] = override_get_session
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        yield ac
