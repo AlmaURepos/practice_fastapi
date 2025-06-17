47331c4ea49b921322fd8990537351d088bbcf8d
amangeldiaidos660 <amangeldiaidos660@gmail.com>
2025-06-17 14:15:33 +0500

    github ci/cd test

--- Амангелді_Айдос_Задание_15/tests/conftest.py
+++ Амангелді_Айдос_Задание_15/tests/conftest.py
@@ -1,34 +1,41 @@
-import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
 import pytest_asyncio
-from httpx import AsyncClient, ASGITransport
+from httpx import AsyncClient
+from httpx._transports.asgi import ASGITransport
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from app import app  
-from database import Base, get_session  
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-TEST_DB_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost/test_notes_db")
+from app import app
+from database import Base, get_session
 
-engine_test = create_async_engine(TEST_DB_URL, echo=False, future=True)
-TestingSessionLocal = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+TEST_DB_URL = os.environ.get("DATABASE_URL")
+
+
+test_engine = create_async_engine(TEST_DB_URL, echo=False, future=True)
+test_async_session = sessionmaker(test_engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def override_get_session():
-    async with TestingSessionLocal() as session:
+    async with test_async_session() as session:
         yield session
 
 app.dependency_overrides[get_session] = override_get_session
 
-@pytest_asyncio.fixture(scope="function", autouse=True)
-async def prepare_database():
-    async with engine_test.begin() as conn:
+
+@pytest_asyncio.fixture(scope='function', autouse=True)
+async def setup_db():
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with engine_test.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
 
 @pytest_asyncio.fixture(scope="function")
 async def client():
-    async with AsyncClient(app=app, base_url="http://test") as c:
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as c:
         yield c
